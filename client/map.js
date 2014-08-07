83657be155437819c8e8c381f9d968df12192c3d
Remco Veldkamp <remcoder@gmail.com>
2014-08-07 14:17:04 +0200

    map editor

--- client/map.js
+++ client/map.js
@@ -2,6 +2,38 @@ Template.map.helpers({
 	rows : function() {
 		var map = Map.findOne('GsPYMGi5kpxAPZTpg');
 		if (!map) return [];
-		return map.rows;
+		return map.rows.map( function(row, index) {
+			return {
+				row: row.map(function(tile, index) {
+					return { tile: tile, index: index };
+				}),
+				index: index
+			};
+		});
+	}
+});
+
+Template.map.events({
+	'click .tile' : function(e) {
+		var y = e.currentTarget.getAttribute('data-y');
+		var x = e.currentTarget.getAttribute('data-x');
+
+		console.log('click', x, y);
+		var map = Map.findOne('GsPYMGi5kpxAPZTpg');
+
+		var oldValue = map.rows[y][x];
+
+		if (oldValue == 'floor')
+			var newValue = 'wall';
+		else
+			var newValue = 'floor';
+
+		map.rows[y][x]  = newValue;
+
+		Map.update('GsPYMGi5kpxAPZTpg', {
+			$set : {
+				rows : map.rows
+			}
+		});
 	}
 });
